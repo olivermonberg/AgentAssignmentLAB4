047b17f579e9dd210a356d3332e75f5e95230160
Oliver Monberg <au554208@uni.au.dk>
2018-09-19 10:02:29 +0200

    Add project files.

---
+++ AgentAssignmentLAB4/Agents.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmFoundation.Wpf;
+using System.Windows.Input;
+using System.Runtime.CompilerServices;
+
+namespace AgentAssignment
+{
+    // 
+    public class Agents : ObservableCollection<Agent>, INotifyPropertyChanged
+    {
+        public Agents()
+        {
+            Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
+            Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+        }
+
+        #region Commands
+
+        ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new RelayCommand(AddAgent)); }
+        }
+
+        private void AddAgent()
+        {
+            Add(new Agent());
+            CurrentIndex = Count - 1;
+        }
+
+
+        ICommand _deleteCommand;
+        public ICommand DeleteCommand
+        {
+            get { return _deleteCommand ?? (_deleteCommand = new RelayCommand(DeleteAgent, DeleteAgent_CanExecute)); }
+        }
+
+        private void DeleteAgent()
+        {
+            RemoveAt(CurrentIndex);
+        }
+
+        private bool DeleteAgent_CanExecute()
+        {
+            if (Count > 0 && CurrentIndex >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        ICommand _nextCommand;
+        public ICommand NextCommand
+        {
+            get
+            {
+                return _nextCommand ?? (_nextCommand = new RelayCommand(
+                    () => ++CurrentIndex,
+                    () => CurrentIndex < (Count - 1)));
+            }
+        }
+
+        ICommand _PreviusCommand;
+        public ICommand PreviusCommand
+        {
+            get { return _PreviusCommand ?? (_PreviusCommand = new RelayCommand(PreviusCommandExecute, PreviusCommandCanExecute)); }
+        }
+
+        private void PreviusCommandExecute()
+        {
+            if (CurrentIndex > 0)
+                --CurrentIndex;
+        }
+
+        private bool PreviusCommandCanExecute()
+        {
+            if (CurrentIndex > 0)
+                return true;
+            else
+                return false;
+        }
+
+        #endregion // Commands
+
+        #region Properties
+
+        int currentIndex = -1;
+
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set
+            {
+                if (currentIndex != value)
+                {
+                    currentIndex = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged implementation
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
